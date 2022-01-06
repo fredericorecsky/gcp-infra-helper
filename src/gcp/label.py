9fc0a62e3d51a16cd55ea74a0f5d4b0d2a8b6437
Frederico Fernandes Recsky <cartas@frederico.me>
2022-01-06 15:30:15 +0100

    returns the buckets for the given labels

--- src/gcp/label.py
+++ src/gcp/label.py
@@ -1,3 +1,54 @@
 
+import sys
+from google.cloud import storage
+
+def list_buckets_by_label( labels ):
+    client = storage.Client()
+    buckets = client.list_buckets()
+
+    output = list()
+    for bucket in buckets:
+        if match_labels( bucket, labels ):
+            output.append( bucket.name )
+        else:
+            next                
+    
+    return output
+    
+def match_labels( entity, labels ):
+    for name in labels:
+        if entity.labels.get( name ) != labels[name]:            
+            return False
+               
+    return True
+
 def help():
-    return """I get labels for gcp resources"""
+    return """Return the buckets names that matchs a set of labels"""
+
+
+# Always for storage now
+
+def get_label():   
+    args = sys.argv
+    script = args.pop(0)
+    entity = args.pop(0)
+
+    labels = {}
+
+    for arg in args:
+        name, value = arg.split('=')
+        labels[name] = value
+        
+    output = list_buckets_by_label( labels )
+
+    if len( output ) == 1:
+        print( output.pop())
+    else:
+        print( output )   
+
+    sys.exit()
+
+
+if __name__ == '__main__':
+    get_label()
+
